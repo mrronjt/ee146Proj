bd2de5da3889637f518e6216836d4d9c73c1cfb3
Ethan <ethan@DESKTOP-92ALJTC.localdomain>
2019-03-11 23:36:21 -0700

    Updating naiveDetector directory

--- naiveDetector/original.py
+++ naiveDetector/original.py
@@ -1,6 +1,5 @@
 import cv2 as cv
 import numpy as np
-# import matplotlib.pyplot as plt
 
 def do_canny(frame):
     # Converts frame to grayscale because we only need the luminance channel for detecting edges - less computationally expensive
@@ -81,21 +80,22 @@ while (cap.isOpened()):
     ret, frame = cap.read()
     canny = do_canny(frame)
     cv.imshow("canny", canny)
-    # plt.imshow(frame)
-    # plt.show()
+    
     segment = do_segment(canny)
+    
     hough = cv.HoughLinesP(segment, 2, np.pi / 180, 100, np.array([]), minLineLength = 100, maxLineGap = 50)
     # Averages multiple detected lines from hough into one line for left border of lane and one line for right border of lane
     lines = calculate_lines(frame, hough)
     #print(lines)
+    
     # Visualizes the lines
     lines_visualize = visualize_lines(frame, lines)
     cv.imshow("hough", lines_visualize)
+    
     # Overlays lines on frame by taking their weighted sums and adding an arbitrary scalar value of 1 as the gamma argument
     output = cv.addWeighted(frame, 0.9, lines_visualize, 1, 1)
-    # Opens a new window and displays the output frame
     cv.imshow("output", output)
-    # Frames are read by intervals of 10 milliseconds. The programs breaks out of the while loop when the user presses the 'q' key
+    
     if cv.waitKey(10) & 0xFF == ord('q'):
         break
 # The following frees up resources and closes all windows
